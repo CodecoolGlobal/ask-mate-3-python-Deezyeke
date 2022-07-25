db4722a45e55e0728acaef8ad339c8145efe4c03
MiMi0001 <willijiem@gmail.com>
2022-07-25 20:56:05 +0200

    Implement List questions and Add questions

---
+++ data_operations.py
@@ -0,0 +1,34 @@
+import csv
+import os
+from collections import OrderedDict
+
+FILENAME_QUESTIONS = 'question.csv'
+QUESTION_HEADER = ['id', 'timestamp', 'text']
+
+
+def create_id():
+    return os.urandom(4).hex()
+
+def load_questions():
+    questions = OrderedDict()
+    try:
+        with open(FILENAME_QUESTIONS, 'r') as f:
+            f_csv = csv.DictReader(f, delimiter = ',')
+            for row in f_csv:
+                question ={}
+                for key in row.keys():
+                    if key != 'id':
+                        question[key] = row[key]
+                questions[row['id']] = question
+            return questions
+    except FileNotFoundError:
+        with open(FILENAME_QUESTIONS, 'w') as f:
+            f_csv = csv.DictWriter(f, fieldnames = QUESTION_HEADER)
+            f_csv.writeheader()
+            return questions
+
+
+def save_question(question):
+    with open(FILENAME_QUESTIONS, 'a') as f:
+        f_csv = csv.DictWriter(f, fieldnames = QUESTION_HEADER)
+        f_csv.writerow(question)
--- server.py
+++ server.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask,render_template, request, redirect
+import data_operations
+from datetime import datetime
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/add_question', methods=['GET', 'POST'])
+def add_question():
+    if request.method == 'GET':
+        return render_template('add_question.html')
+    elif request.method == 'POST':
+        question ={}
+        now = datetime.now()
+        question['id'] = data_operations.create_id()
+        question['timestamp'] = now.strftime("%Y/%m/%d %H:%M:%S")
+        question['text'] = request.form.get('text')
+        data_operations.save_question(question)
+
+## !!!  Ezt át kell majd írni   !!!
+        return redirect('/list')
+
+
+@app.route('/')
+@app.route('/list')
+def list():
+    questions = data_operations.load_questions()
+    return render_template('list.html', questions = questions)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        host='0.0.0.0',
+        port=5000,
+        debug=True,
+    )
+
