4efc6486a8b1da4f1b0800d097d60121de1beaec
SzimBensze <bence.smile.he@gmail.com>
2022-07-28 11:47:43 +0200

    Added vote buttons function

--- server.py
+++ server.py
@@ -39,22 +39,22 @@ def questions_and_answers(id):
     return redirect('display_question.html')
 
 
-@app.route('/question/<question_id>/vote-up')
+@app.route('/question/<question_id>/vote-up', methods=['POST'])
 def question_vote_up(question_id):
     questions = util.read_questions()
     data = util.choose_data(question_id)
     current_vote_number = util.change_votenum(data, "+")
     util.update_data(questions, question_id, "vote_number", current_vote_number, "question")
-    return redirect('/', question_id=question_id)
+    return redirect('/')
 
 
-@app.route('/question/<question_id>/vote-down')
+@app.route('/question/<question_id>/vote-down', methods=['POST'])
 def question_vote_down(question_id):
     questions = util.read_questions()
     data = util.choose_data(question_id)
     current_vote_number = util.change_votenum(data, "-")
     util.update_data(questions, question_id, "vote_number", current_vote_number, "question")
-    return redirect('/', question_id=question_id)
+    return redirect('/')
 
 
 @app.route('/questions/<id>/delete')
