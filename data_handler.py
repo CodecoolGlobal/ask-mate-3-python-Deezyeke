2ce07e5e431dd46faa15ed404f06567b753ae426
MiMi0001 <willijiem@gmail.com>
2022-08-09 13:51:04 +0200

    Refactor Add question for SQL

--- data_handler.py
+++ data_handler.py
@@ -19,10 +19,11 @@ def get_all_questions(cursor):
     cursor.execute(query)
     return cursor.fetchall()
 
+
 @connection_handler
 def save_new_question(cursor, question):
     query = """
-    INSERT INTO QUESTION ('submission_time', 'view_number', 'vote_number', 'title', 'message', 'image')
+    INSERT INTO question (submission_time, view_number, vote_number, title, message, image)
     VALUES ( %(st)s, %(vi)s, %(vo)s, %(ti)s, %(me)s, %(im)s )"""
     cursor.execute(query, {'st': question['submission_time'], 'vi': question['view_number'], 'vo': question['view_number'], 'ti': question['title'],
                            'me': question['message'], 'im': question['image']})
@@ -35,3 +36,11 @@ def get_all_answers(cursor):
         FROM answer"""
     cursor.execute(query)
     return cursor.fetchall()
+
+
+def create_empty_question():
+    question = {}
+    question['view_number'] = 0
+    question['vote_number'] = 0
+    question['image'] = None
+    return question
--- server.py
+++ server.py
@@ -15,19 +15,12 @@ def index():
     return render_template('questions_list.html', orderby='id', questions=questions, question_header=data_handler.QUESTION_HEADER)
 
 
-def create_empty_question():
-    question = {}
-    question['view_number'] = 0
-    question['vote_number'] = 0
-    return question
-
-
 @app.route('/add_question', methods=['GET', 'POST'])
 def add_question():
     if request.method == 'GET':
         return render_template('add_question.html')
     elif request.method == 'POST':
-        question = data_operations.create_empty_question()
+        question = data_handler.create_empty_question()
         now = datetime.now()
         question['submission_time'] = now.strftime("%Y/%m/%d %H:%M:%S")
         question['title'] = request.form.get('title')
@@ -37,9 +30,9 @@ def add_question():
             uploaded_file.save(os.path.join('static', uploaded_file.filename))
             question['image'] = uploaded_file.filename
 
-        connection.save_new_question()
+        data_handler.save_new_question(question)
 
-        return redirect('/list')
+        return redirect(url_for('index'))
 
 
 @app.route('/display-question/<q_id>')
